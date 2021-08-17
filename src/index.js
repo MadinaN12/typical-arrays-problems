8745852027dd4a80f47b76cabd2b06bab1548975
Madina <nishanbaeva.m.s.95@gmail.com>
2021-08-17 19:35:29 +0500

    init: typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array == undefined || array.length === 0) return 0;
+  var minm = array[0];
+
+  for (var elem of array) {
+    if (minm > elem) { minm = elem; }
+  }
+  
+  return minm;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array == undefined || array.length === 0) return 0;
+  var maxm = array[0];
+
+  for (var elem of array) {
+    if (maxm < elem) { maxm = elem; }
+  }
+  
+  return maxm;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == undefined ) return 0;
+  var avr = 0;
+  for (var i = 0; i < array.length; i++) {
+    avr += array[i];
+  }
+  if (avr !== 0) avr = avr / array.length;
+  return avr;
 }
